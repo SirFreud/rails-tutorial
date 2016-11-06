2313d36904d9541ad03c242cc20b5e39d1a904c0
SirFreud <mikevaldez92@gmail.com>
2016-11-06 13:40:48 -0800

    Commented out goodbye route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
-  get 'application#goodbye'
+  # get 'application#goodbye'
 end
